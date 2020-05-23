ce735e9be8a5d5fc45cb712bae49f92e24709b97
AlexRosso1025 <31864275+AlexRosso1025@users.noreply.github.com>
2020-05-22 21:52:11 -0500

    Declare a variable to check pmd 3.0

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	int dinamismo;
+	int julio = dinamismo;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
